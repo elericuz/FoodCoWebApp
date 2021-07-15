d7e9d27eb93dd51db71ff7eeaea519de38194a8e
Eric Valera Miller <eric@elericuz.com>
2021-07-14 23:39:52 -0500

    invoices to PDF

--- api/routes/invoices.js
+++ api/routes/invoices.js
@@ -5,5 +5,6 @@ const checkAuth = require('../middleware/check-auth');
 const InvoiceController = require('../controllers/invoices')
 
 router.post('/save', checkAuth, InvoiceController.save);
+router.get('/download/:id', checkAuth, InvoiceController.downloadInvoice);
 
 module.exports = router;
