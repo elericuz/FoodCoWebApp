82363db85687c3975ad282ac4000dfdd1ec1f5c0
Eric Valera Miller <eric@elericuz.com>
2021-07-15 23:16:30 -0500

    mongo settings

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const server = http.createServer(app);
 // connect mongodb
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-const dbUri = "mongodb+srv://foodCo:" + process.env.MONGO_ATLAS_PW + "@cluster0.zlz63.mongodb.net/foodCo-db?retryWrites=true&w=majority"
+
+let dbUri = "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_ATLAS_PW + "@" + process.env.MONGO_SERVER + ":" + process.env.MONGO_PORT + "/" + process.env.MONGO_DB + "?retryWrites=true&w=majority"
+if (process.env.ENVIRONMENT=="DEVELOPMENT") {
+    dbUri = "mongodb+srv://" + process.env.MONGO_USER_DEV + ":" + process.env.MONGO_PASSWORD_DEV + "@" + process.env.MONGO_SERVER_DEV + "/" + process.env.MONGO_DB_DEV + "?retryWrites=true&w=majority"
+}
 mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => server.listen(PORT, HOST))
     .catch((err) => console.log(err));
